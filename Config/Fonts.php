59bb2a935b85c923900c3cda9a81bb44a9fcc889
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-16 06:32:31 +0300

    no message

--- Config/Fonts.php
+++ Config/Fonts.php
@@ -1,4 +1,13 @@
 <?php
 namespace Dfe\Typography\Config;
 use Df\Framework\Data\Form\Element\Table;
-class Fonts extends Table {}
+class Fonts extends Table {
+	/**
+	 * 2015-12-16
+	 * @override
+	 * @see \Df\Framework\Data\Form\Element\Table::columns()
+	 * @used-by \Df\Framework\Data\Form\Element\Table::onFormInitialized()
+	 * @return string[]
+	 */
+	protected function columns() {return ['Column 1', 'Column 2', 'Column 3'];}
+}
